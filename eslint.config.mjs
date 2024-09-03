d0e15b32cc3b6ed54db655aa9c473df8749acf19
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-03 17:16:47 +0200

    chore: install and configure eslint-plugin-unicorn package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import globals from "globals"
+import globals from "globals";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 export default tseslint.config({
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended, eslintPluginUnicorn.configs["flat/recommended"]],
   languageOptions: {
     globals: globals.node
   },
