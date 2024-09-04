8347536a3f6bfcbf62e906766502b41e777920ed
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-04 08:48:45 +0200

    feat: create global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import CustomError from "./utils/custom-error";
 
 const app = express();
+
+app.use(
+  (
+    error: CustomError,
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+  ) => {
+    const { message, errors, statusCode } = error;
+
+    response.status(statusCode || 500).json({ message: errors || message });
+  },
+);
 
 // eslint-disable-next-line no-console
 app.listen(5000, () => console.log("Server: http://localhost:5000"));
