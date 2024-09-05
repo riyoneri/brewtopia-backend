f8ca60423e1ad0f0633d724139564118bf147615
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-05 20:22:37 +0200

    feat: validate admin/auth/google endpoint

--- src/routes/admin-auth.ts
+++ src/routes/admin-auth.ts
@@ -6,42 +6,65 @@ import { Admin } from "../models";
 
 const router = express.Router();
 
-router.post(
-  "/register",
-  [
-    body("name", "Name is required")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .trim(),
-    body("email", "Email is required")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .isEmail()
-      .withMessage("Email must be valid")
-      .trim()
-      .normalizeEmail({ all_lowercase: true })
-      .bail()
-      .custom((value) =>
-        Admin.findOne({ "email.value": value }).then((admin) => {
-          if (admin) throw "Email already exists";
-        }),
-      ),
-    body("password", "Password is required")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .isStrongPassword({
-        minLength: 8,
-        minLowercase: 1,
-        minNumbers: 1,
-        minSymbols: 1,
-        minUppercase: 1,
-      })
-      .withMessage("Password is not strong enough"),
-    body("confirmPassword", "Confirm password is required")
-      .custom((value, { req }) => req.body.password === value)
-      .withMessage("Passwords must match"),
-  ],
-  adminAuthController.createAdmin,
-);
+router
+  .post(
+    "/google",
+    [
+      body("name", "Name is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("email", "Email is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isEmail()
+        .withMessage("Email must be valid")
+        .trim()
+        .normalizeEmail({ all_lowercase: true }),
+      body("image", "Image must be valid url")
+        .optional({ values: "undefined" })
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+    ],
+    adminAuthController.authWithGoogle,
+  )
+  .post(
+    "/register",
+    [
+      body("name", "Name is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("email", "Email is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isEmail()
+        .withMessage("Email must be valid")
+        .trim()
+        .normalizeEmail({ all_lowercase: true })
+        .bail()
+        .custom((value) =>
+          Admin.findOne({ "email.value": value }).then((admin) => {
+            if (admin) throw "Email already exists";
+          }),
+        ),
+      body("password", "Password is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isStrongPassword({
+          minLength: 8,
+          minLowercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+          minUppercase: 1,
+        })
+        .withMessage("Password is not strong enough"),
+      body("confirmPassword", "Confirm password is required")
+        .custom((value, { req }) => req.body.password === value)
+        .withMessage("Passwords must match"),
+    ],
+    adminAuthController.createAdmin,
+  );
 
 export default router;
