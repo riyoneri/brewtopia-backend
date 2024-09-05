bfbc65556aca7fef4668e07daf077142be3952e8
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-05 09:26:06 +0200

    feat: connect app to database

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,19 @@ import cors from "cors";
 import { config } from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
+import { connect } from "mongoose";
 import morgan from "morgan";
 import { debug } from "node:console";
+import { exit } from "node:process";
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
 import appRouter from "./routes";
 import CustomError from "./utils/custom-error";
 
 config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -54,13 +59,20 @@ app.use(
   },
 );
 
-const server = app.listen(5000, () =>
-  // eslint-disable-next-line no-console
-  console.log("Server: http://localhost:5000"),
-);
+if (MONGODB_URL)
+  connect(MONGODB_URL)
+    .then(() => {
+      const server = app.listen(PORT, () =>
+        // eslint-disable-next-line no-console
+        console.log(`Server: http://localhost:${5000}`),
+      );
 
-process.on("SIGTERM", () => {
-  server.close(() => {
-    debug("HTTP server closed");
-  });
-});
+      process.on("SIGTERM", () => {
+        server.close(() => {
+          debug("HTTP server closed");
+        });
+      });
+    })
+    // eslint-disable-next-line unicorn/prefer-top-level-await
+    .catch(() => exit(1));
+else exit(1);
