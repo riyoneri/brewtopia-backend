52b2e79e850853a4c0708d875d2f132b5245248f
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-06 12:29:31 +0200

    refactor: export app as default object

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,13 @@
 import bodyParser from "body-parser";
 import compression from "compression";
 import cors from "cors";
-import { config } from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
-import { connect } from "mongoose";
 import morgan from "morgan";
-import { debug } from "node:console";
-import { exit } from "node:process";
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
 import appRouter from "./routes";
 import CustomError from "./utils/custom-error";
-
-config();
-
-const MONGODB_URL = process.env.MONGODB_URL;
-const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -59,18 +50,4 @@ app.use(
   },
 );
 
-connect(MONGODB_URL!)
-  // eslint-disable-next-line unicorn/prefer-top-level-await
-  .then(() => {
-    const server = app.listen(PORT, () =>
-      // eslint-disable-next-line no-console
-      console.log(`Server: http://localhost:${5000}`),
-    );
-
-    process.on("SIGTERM", () => {
-      server.close(() => {
-        debug("HTTP server closed");
-      });
-    });
-  })
-
+export default app;
