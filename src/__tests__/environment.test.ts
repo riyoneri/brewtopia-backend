946118da878d67642a4b0b80f92b1ccee82c11e9
Lionel Kaneza <lionkaneza@gmail.com>
2024-09-06 17:31:46 +0200

    refactor: add RESEND_API_KEY in environment variables

--- src/__tests__/environment.test.ts
+++ src/__tests__/environment.test.ts
@@ -5,7 +5,8 @@ config();
 describe("Environment variables", () => {
   test("Should return true if environment variables are available", async () => {
     const MONGODB_URL = process.env.MONGODB_URL;
+    const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
-    expect(MONGODB_URL).toBeTruthy();
+    expect(MONGODB_URL && RESEND_API_KEY).toBeTruthy();
   });
 });
