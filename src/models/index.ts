09fba577aab9766048b84abd507983362b2137fb
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-09 23:58:56 +0200

    feat: create User schema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export { default as Admin } from "./admin.model";
+export { default as User } from "./user.model";
---
+++ src/models/user.model.ts
@@ -0,0 +1,80 @@
+import { Schema, model } from "mongoose";
+
+interface IUser {
+  name: string;
+  email: {
+    value: string;
+    verified: boolean;
+  };
+  password?: string;
+  imageUrl?: string;
+  authTokens: {
+    emailVerification?: string;
+    resetPassword?: {
+      value: string;
+      expirationDate: string;
+    };
+  };
+  active: boolean;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: {
+        value: {
+          type: String,
+          required: true,
+        },
+        verified: {
+          type: Boolean,
+          required: true,
+          default: false,
+        },
+      },
+      required: true,
+      _id: false,
+    },
+    password: String,
+    imageUrl: String,
+    active: { type: Boolean, required: true, default: true },
+    authTokens: {
+      emailVerification: String,
+      resetPassword: {
+        type: {
+          value: String,
+          expirationDate: Date,
+        },
+      },
+      _id: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      updatedAt: false,
+    },
+    toJSON: {
+      transform(document, returnValue) {
+        delete returnValue.password;
+        delete returnValue._id;
+        delete returnValue.authTokens;
+        delete returnValue.active;
+
+        return {
+          id: document.id,
+          ...returnValue,
+          email: returnValue.email.value,
+          imageUrl: returnValue?.imageUrl?.includes("https")
+            ? returnValue?.imageUrl
+            : returnValue.imageUrl
+              ? `${process.env.AWS_DISTRIBUTION_DOMAIN_NAME}/${returnValue.imageUrl}`
+              : undefined,
+        };
+      },
+    },
+  },
+);
+
+export default model<IUser>("User", userSchema);
