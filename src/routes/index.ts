aac8e4a2039e5140a5c4890277fe5d70ed0cc5d5
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-09 23:59:36 +0200

    feat: create auth with google route[client]

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import adminAuthRoute from "./admin-auth.route";
+import userAuthRouter from "./user-auth.router";
 
 const router = Router();
 
 router.use("/admin/auth", adminAuthRoute);
+router.use("/auth", userAuthRouter);
 
 export default router;
