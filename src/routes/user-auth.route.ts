bec11a15d52263b597c4cba75cf4930ac2fbe4a7
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-10 14:21:50 +0200

    feat: create and validate user register route

--- src/routes/user-auth.route.ts
+++ src/routes/user-auth.route.ts
@@ -2,43 +2,91 @@ import { Router } from "express";
 import { body, header } from "express-validator";
 
 import { userAuthController } from "../controllers";
+import { User } from "../models";
 
 const router = Router();
 
-router.post(
-  "/google",
-  [
-    body("name", "Name is required")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .trim(),
-    body("email", "Email must be valid google mail")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .isEmail({ host_whitelist: ["gmail.com"] })
-      .trim()
-      .normalizeEmail({ all_lowercase: true }),
-    body("picture", "Image must be valid google image url")
-      .isString()
-      .optional({ values: "undefined" })
-      .isURL({ host_whitelist: ["lh3.googleusercontent.com"] })
-      .notEmpty({ ignore_whitespace: true })
-      .trim(),
-    header("authorization", "Callback token is required")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .bail()
-      .custom((value) => {
-        try {
-          const callbackToken = value.split(" ")[1];
-          return process.env.CALLBACK_TOKEN === callbackToken;
-        } catch {
-          return false;
-        }
-      })
-      .withMessage("Callback token is invalid"),
-  ],
-  userAuthController.authWithGoogle,
-);
+router
+  .post(
+    "/google",
+    [
+      body("name", "Name is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("email", "Email must be valid google mail")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isEmail({ host_whitelist: ["gmail.com"] })
+        .trim()
+        .normalizeEmail({ all_lowercase: true }),
+      body("picture", "Image must be valid google image url")
+        .isString()
+        .optional({ values: "undefined" })
+        .isURL({ host_whitelist: ["lh3.googleusercontent.com"] })
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      header("authorization", "Callback token is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .bail()
+        .custom((value) => {
+          try {
+            const callbackToken = value.split(" ")[1];
+            return process.env.CALLBACK_TOKEN === callbackToken;
+          } catch {
+            return false;
+          }
+        })
+        .withMessage("Callback token is invalid"),
+    ],
+    userAuthController.authWithGoogle,
+  )
+  .post(
+    "/register",
+    [
+      body("name", "Name is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("email", "Email is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isEmail()
+        .withMessage("Email must be valid")
+        .trim()
+        .normalizeEmail({ all_lowercase: true })
+        .bail()
+        .custom((value) =>
+          User.findOne({ "email.value": value }).then((user) => {
+            if (user) throw "Email already exists";
+          }),
+        ),
+      body("password", "Password is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isStrongPassword({
+          minLength: 8,
+          minLowercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+          minUppercase: 1,
+        })
+        .withMessage("Password is not strong enough"),
+      body("confirmPassword", "Confirm password is required")
+        .custom((value, { req }) => req.body.password === value)
+        .withMessage("Passwords must match"),
+      body("redirectUrl", "Redirect url is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isURL({
+          require_protocol: true,
+          allow_query_components: false,
+          require_tld: process.env.NODE_ENV === "production",
+        })
+        .withMessage("Redirect url must be a valid url"),
+    ],
+    userAuthController.createUser,
+  );
 
 export default router;
