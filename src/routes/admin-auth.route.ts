bc9a38f5cca89cc58f4ffc1a06a9edb8c9ac5e0a
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-11 19:05:06 +0200

    feat: create admin reset passsword route

--- src/routes/admin-auth.route.ts
+++ src/routes/admin-auth.route.ts
@@ -142,6 +142,30 @@ router
         .withMessage("Redirect url must be a valid url"),
     ],
     adminAuthController.forgotPassword,
+  )
+  .post(
+    "/reset-password",
+    [
+      body("token", "Token is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("password", "Password is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isStrongPassword({
+          minLength: 8,
+          minLowercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+          minUppercase: 1,
+        })
+        .withMessage("Password is not strong enough"),
+      body("confirmPassword", "Confirm password is required")
+        .custom((value, { req }) => req.body.password === value)
+        .withMessage("Passwords must match"),
+    ],
+    adminAuthController.resetPassword,
   );
 
 export default router;
