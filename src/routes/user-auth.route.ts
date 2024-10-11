9cd9e13b55d5cdd9ae416f9cecf1f56cef26fdea
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-11 18:09:12 +0200

    feat: create and validate user reset password route

--- src/routes/user-auth.route.ts
+++ src/routes/user-auth.route.ts
@@ -141,6 +141,30 @@ router
         .withMessage("Redirect url must be a valid url"),
     ],
     userAuthController.forgotPassword,
+  )
+  .post(
+    "/reset-password",
+    [
+      body("token", "Token is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .trim(),
+      body("password", "Password is required")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isStrongPassword({
+          minLength: 8,
+          minLowercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+          minUppercase: 1,
+        })
+        .withMessage("Password is not strong enough"),
+      body("confirmPassword", "Confirm password is required")
+        .custom((value, { req }) => req.body.password === value)
+        .withMessage("Passwords must match"),
+    ],
+    userAuthController.resetPassword,
   );
 
 export default router;
