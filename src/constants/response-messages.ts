c2ad3a895d914b064317c3543209e00690fbe793
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-11 12:43:38 +0200

    feat: create response messages constants

---
+++ src/constants/response-messages.ts
@@ -0,0 +1,5 @@
+export const ServerErrorMessage = "Internal server error";
+
+export const ValidationErrorMessage = "Validation error";
+
+export const getNotFoundMessage = (item: string) => `${item} not found`;
