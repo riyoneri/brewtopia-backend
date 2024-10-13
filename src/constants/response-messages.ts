465921b23cb762ebb8666c689293df5df67869b1
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-13 15:04:49 +0200

    feat: create InvalidCredentialsMessage constant

--- src/constants/response-messages.ts
+++ src/constants/response-messages.ts
@@ -2,4 +2,6 @@ export const ServerErrorMessage = "Internal server error";
 
 export const ValidationErrorMessage = "Validation error";
 
+export const InvalidCredentialsMessage = "Incorrect email or password";
+
 export const getNotFoundMessage = (item: string) => `${item} not found`;
