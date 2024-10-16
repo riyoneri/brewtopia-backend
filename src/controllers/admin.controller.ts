1818192a555a7c1a55c5c9d9a1411bd03ea0e883
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-16 19:52:27 +0200

    feat: create list customers controller

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,15 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 
 import { ServerErrorMessage } from "../constants";
+import { User } from "../models";
 import CustomError from "../utils/custom-error";
 
 export const listCustomers = async (
-  _request: Request,
+  request: Request,
   response: Response,
   next: NextFunction,
 ) => {
   try {
-    response.status(200).json({ message: "You reached here" });
+    const page = Number(request.query.page) || 1;
+    const limit = Number(request.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const totalUsers = await User.countDocuments();
+
+    const users = await User.find({}, undefined, {})
+      .skip(skip)
+      .limit(limit)
+      .transform((documents) =>
+        documents.map((document) => ({
+          ...document.toJSON(),
+          active: document.active,
+        })),
+      );
+
+    response.status(200).json({ users: users, total: totalUsers });
   } catch {
     const error = new CustomError(ServerErrorMessage);
     next(error);
