d4ad737dd9a95251b49a114e3818c894dcf911e9
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-17 05:33:37 +0200

    feat: protect admin routes

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { adminController } from "../controllers";
+import adminAuthMiddleware from "../middlewares/admin-auth.middleware";
 
 const router = Router();
 
+router.use(adminAuthMiddleware);
 router.get("/clients", adminController.listCustomers);
 
 export default router;
