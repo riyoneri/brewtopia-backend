535edc96e6141a1b023341267c3680fd26690ee1
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-17 05:31:57 +0200

    feat: create invalid auth user message constant

--- src/constants/response-messages.ts
+++ src/constants/response-messages.ts
@@ -4,4 +4,6 @@ export const ValidationErrorMessage = "Validation error";
 
 export const InvalidCredentialsMessage = "Incorrect email or password";
 
+export const InvalidAuthUserMessage = "Invalid auth user";
+
 export const getNotFoundMessage = (item: string) => `${item} not found`;
