1c08f3bcbf2a043378f5f620fe393e143c11697e
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-20 04:30:36 +0200

    feat: create and validate update-user-status endpoint

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
 
 import { adminController } from "../controllers";
 import adminAuthMiddleware from "../middlewares/admin-auth.middleware";
@@ -7,5 +8,18 @@ const router = Router();
 
 router.use(adminAuthMiddleware);
 router.get("/clients", adminController.listCustomers);
+router.post(
+  "/clients/:userId/status",
+  [
+    param("userId", "Enter a valid user id")
+      .isString()
+      .notEmpty({ ignore_whitespace: true })
+      .isMongoId(),
+    body("active", "Active property must be a boolean").isBoolean({
+      strict: true,
+    }),
+  ],
+  adminController.changeClientStatus,
+);
 
 export default router;
