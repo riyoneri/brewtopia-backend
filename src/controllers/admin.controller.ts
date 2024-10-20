8e1deb27c8899b300d0f5181eb6f5897ed4cbdcc
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-20 04:31:08 +0200

    feat: create update-user-status controller

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 
-import { ServerErrorMessage } from "../constants";
+import {
+  ServerErrorMessage,
+  ValidationErrorMessage,
+  getNotFoundMessage,
+} from "../constants";
+import getCustomValidationResults from "../helpers/get-validation-results";
 import { User } from "../models";
+import { clientSocket } from "../sockets";
 import CustomError from "../utils/custom-error";
 
 export const listCustomers = async (
@@ -27,6 +33,51 @@ export const listCustomers = async (
       );
 
     response.status(200).json({ users: users, total: totalUsers });
+  } catch {
+    const error = new CustomError(ServerErrorMessage);
+    next(error);
+  }
+};
+
+export const changeClientStatus = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  try {
+    const validationErrors = getCustomValidationResults(request);
+
+    if (validationErrors) {
+      const error = new CustomError(
+        ValidationErrorMessage,
+        400,
+        validationErrors,
+      );
+      return next(error);
+    }
+
+    const user = await User.findOne({ _id: request.params.userId });
+
+    if (!user) {
+      const error = new CustomError(getNotFoundMessage("User"), 404);
+
+      return next(error);
+    }
+
+    if (user.active === request.body.active)
+      return response
+        .status(200)
+        .json({ message: "User status was updates successfully" });
+
+    user.active = request.body.active;
+
+    await user.save();
+
+    clientSocket.changeStatus(user.id, request.body.active);
+
+    response
+      .status(200)
+      .json({ message: "User status was updates successfully" });
   } catch {
     const error = new CustomError(ServerErrorMessage);
     next(error);
