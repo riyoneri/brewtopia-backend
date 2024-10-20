c1c55eeafee6f0557d41d9a83b8733c874319286
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-20 04:26:49 +0200

    refactor: make unused imports error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default tseslint.config({
     "no-console": "error",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-imports": "error",
     "unused-imports/no-unused-vars": [
       "error",
       {
