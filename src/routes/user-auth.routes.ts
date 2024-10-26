6dd1444cc988ea267ea4526fe7dea981339513cd
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-26 13:51:00 +0200

    feat: user-auth-routes to external validation chains

--- src/routes/user-auth.routes.ts
+++ src/routes/user-auth.routes.ts
@@ -1,188 +1,38 @@
 import { Router } from "express";
-import { body, header } from "express-validator";
 
-import { environment } from "../config";
 import { userAuthController } from "../controllers";
 import { User } from "../models";
+import {
+  authWithGoogleChain,
+  forgotPasswordChain,
+  loginChain,
+  registerChain,
+  resendVerificationEmailChain,
+  resetPasswordChain,
+  verifyEmailChain,
+} from "../validations";
 
 const router = Router();
 
 router
-  .post(
-    "/google",
-    [
-      body("name", "Name is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .trim(),
-      body("email", "Email must be valid google mail")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isEmail({ host_whitelist: ["gmail.com"] })
-        .trim()
-        .normalizeEmail({ all_lowercase: true }),
-      body("picture", "Image must be valid google image url")
-        .isString()
-        .optional({ values: "undefined" })
-        .isURL({ host_whitelist: ["lh3.googleusercontent.com"] })
-        .notEmpty({ ignore_whitespace: true })
-        .trim(),
-      header("authorization", "Callback token is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .bail()
-        .custom((value) => {
-          try {
-            const callbackToken = value.split(" ")[1];
-            return environment.callbackToken === callbackToken;
-          } catch {
-            return false;
-          }
-        })
-        .withMessage("Callback token is invalid"),
-    ],
-    userAuthController.authWithGoogle,
-  )
-  .post(
-    "/register",
-    [
-      body("name", "Name is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .trim(),
-      body("email", "Email is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isEmail()
-        .withMessage("Email must be valid")
-        .trim()
-        .normalizeEmail({ all_lowercase: true })
-        .bail()
-        .custom((value) =>
-          User.findOne({ "email.value": value }).then((user) => {
-            if (user) throw "Email already exists";
-          }),
-        ),
-      body("password", "Password is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isStrongPassword({
-          minLength: 8,
-          minLowercase: 1,
-          minNumbers: 1,
-          minSymbols: 1,
-          minUppercase: 1,
-        })
-        .withMessage("Password is not strong enough"),
-      body("confirmPassword", "Confirm password is required")
-        .custom((value, { req }) => req.body.password === value)
-        .withMessage("Passwords must match"),
-      body("redirectUrl", "Redirect url is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isURL({
-          require_protocol: true,
-          allow_query_components: false,
-          require_tld: environment.nodeEnv === "production",
-        })
-        .withMessage("Redirect url must be a valid url"),
-    ],
-    userAuthController.createUser,
-  )
+  .post("/google", authWithGoogleChain(), userAuthController.authWithGoogle)
+  .post("/register", registerChain(User), userAuthController.createUser)
   .post(
     "/resend-verification-email",
-    [
-      body("email", "Email is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isEmail()
-        .withMessage("Email must be valid")
-        .trim()
-        .normalizeEmail({ all_lowercase: true }),
-      body("redirectUrl", "Redirect url is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isURL({
-          require_protocol: true,
-          allow_query_components: false,
-          require_tld: environment.nodeEnv === "production",
-        })
-        .withMessage("Redirect url must be a valid url"),
-    ],
+    resendVerificationEmailChain(),
     userAuthController.resendVerificationEmail,
   )
-  .post(
-    "/verify-email",
-    [
-      body("token", "Token is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .trim(),
-    ],
-    userAuthController.verifyEmail,
-  )
+  .post("/verify-email", verifyEmailChain(), userAuthController.verifyEmail)
   .post(
     "/forgot-password",
-    [
-      body("email", "Email is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isEmail()
-        .withMessage("Email must be valid")
-        .trim()
-        .normalizeEmail({ all_lowercase: true }),
-      body("redirectUrl", "Redirect url is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isURL({
-          require_protocol: true,
-          allow_query_components: false,
-          require_tld: environment.nodeEnv === "production",
-        })
-        .withMessage("Redirect url must be a valid url"),
-    ],
+    forgotPasswordChain(),
     userAuthController.forgotPassword,
   )
   .post(
     "/reset-password",
-    [
-      body("token", "Token is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .trim(),
-      body("password", "Password is required")
-        .isString()
-        .notEmpty({ ignore_whitespace: true })
-        .isStrongPassword({
-          minLength: 8,
-          minLowercase: 1,
-          minNumbers: 1,
-          minSymbols: 1,
-          minUppercase: 1,
-        })
-        .withMessage("Password is not strong enough"),
-      body("confirmPassword", "Confirm password is required")
-        .custom((value, { req }) => req.body.password === value)
-        .withMessage("Passwords must match"),
-    ],
+    resetPasswordChain(),
     userAuthController.resetPassword,
   )
-  .post(
-    "/login",
-    [
-      body(
-        ["email", "password"],
-        "Email and Password must be available in body",
-      )
-        .isString()
-        .notEmpty({ ignore_whitespace: true }),
-      body("email")
-        .isString()
-        .isEmail()
-        .trim()
-        .normalizeEmail({ all_lowercase: true }),
-    ],
-    userAuthController.login,
-  );
+  .post("/login", loginChain(), userAuthController.login);
 
 export default router;
