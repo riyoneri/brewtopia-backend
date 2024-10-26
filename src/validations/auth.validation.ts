1fedd9a2743c5e756e3c7634be0d900866e43aa8
Lionel Kaneza <lionkaneza@gmail.com>
2024-10-26 13:39:29 +0200

    feat: create register validation chain

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,4 +1,5 @@
 import { body, header } from "express-validator";
+import { Model } from "mongoose";
 
 import { environment } from "../config";
 
@@ -32,4 +33,47 @@ export const authWithGoogleChain = () => [
       }
     })
     .withMessage("Callback token is invalid"),
+];
+
+export const registerChain = <T>(model: Model<T>) => [
+  body("name", "Name is required")
+    .isString()
+    .notEmpty({ ignore_whitespace: true })
+    .trim(),
+  body("email", "Email is required")
+    .isString()
+    .notEmpty({ ignore_whitespace: true })
+    .isEmail()
+    .withMessage("Email must be valid")
+    .trim()
+    .normalizeEmail({ all_lowercase: true })
+    .bail()
+    .custom((value) =>
+      model.findOne({ "email.value": value }).then((user) => {
+        if (user) throw "Email already exists";
+      }),
+    ),
+  body("password", "Password is required")
+    .isString()
+    .notEmpty({ ignore_whitespace: true })
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+      minUppercase: 1,
+    })
+    .withMessage("Password is not strong enough"),
+  body("confirmPassword", "Confirm password is required")
+    .custom((value, { req }) => req.body.password === value)
+    .withMessage("Passwords must match"),
+  body("redirectUrl", "Redirect url is required")
+    .isString()
+    .notEmpty({ ignore_whitespace: true })
+    .isURL({
+      require_protocol: true,
+      allow_query_components: false,
+      require_tld: environment.nodeEnv === "production",
+    })
+    .withMessage("Redirect url must be a valid url"),
 ];
