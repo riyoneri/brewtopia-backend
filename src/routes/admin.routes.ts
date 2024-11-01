5fbe0d9d4a4c7c4387586464dbdd8d225c1836a6
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-01 11:42:28 +0200

    feat: implement and validate create category endpoint

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,25 +1,33 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
 
-import { adminController } from "../controllers";
+import { categoriesController, clientsController } from "../controllers";
 import adminAuthMiddleware from "../middlewares/admin-auth.middleware";
+import { createCategoryChain } from "../validations";
 
 const router = Router();
 
-router.use(adminAuthMiddleware);
-router.get("/clients", adminController.listCustomers);
+router
+  .use(adminAuthMiddleware)
+  .get("/clients", clientsController.listCustomers)
+  .patch(
+    "/clients/:userId/status",
+    [
+      param("userId", "Enter a valid user id")
+        .isString()
+        .notEmpty({ ignore_whitespace: true })
+        .isMongoId(),
+      body("active", "Active property must be a boolean").isBoolean({
+        strict: true,
+      }),
+    ],
+    clientsController.changeClientStatus,
+  );
+
 router.post(
-  "/clients/:userId/status",
-  [
-    param("userId", "Enter a valid user id")
-      .isString()
-      .notEmpty({ ignore_whitespace: true })
-      .isMongoId(),
-    body("active", "Active property must be a boolean").isBoolean({
-      strict: true,
-    }),
-  ],
-  adminController.changeClientStatus,
+  "/categories",
+  createCategoryChain(),
+  categoriesController.createCategory,
 );
 
 export default router;
