2483e986a2185fadb324ec0cd86bf6fbc4a8756c
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-02 11:24:03 +0200

    refactor: add existence category validation

--- src/validations/admin-categories.validation.ts
+++ src/validations/admin-categories.validation.ts
@@ -1,7 +1,15 @@
 import { body } from "express-validator";
 
+import { Category } from "../models";
+
 export const createCategoryChain = () =>
   body("name", "Name is required")
     .isString()
     .trim()
-    .notEmpty({ ignore_whitespace: true });
+    .notEmpty({ ignore_whitespace: true })
+    .bail()
+    .custom((value) =>
+      Category.findOne({ name: value }).then((category) => {
+        if (category) throw "Category already exist";
+      }),
+    );
