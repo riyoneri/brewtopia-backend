b1e0c7429144ec29c5e8db9bd1c815a661e76ce6
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-03 13:43:58 +0200

    refactor: add name_lower property on category model

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -2,11 +2,13 @@ import { Document, Schema, model } from "mongoose";
 
 export interface ICategory extends Document {
   name: string;
+  name_lower: string;
 }
 
 const categorySchema = new Schema<ICategory>(
   {
     name: { type: String, required: true, unique: true },
+    name_lower: { type: String, required: true, unique: true },
   },
   {
     versionKey: false,
