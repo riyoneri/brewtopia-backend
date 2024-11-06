3441ee360031e08c72108595472ce056ab2ce2ff
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-06 08:52:17 +0200

    refactor: adapt get all categories controller to get-all validation sanitizer

--- src/controllers/admin/categories.controller.ts
+++ src/controllers/admin/categories.controller.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-import { ServerErrorMessage, ValidationErrorMessage } from "../../constants";
+import {
+  ServerErrorMessage,
+  ValidationErrorMessage,
+  getNotFoundMessage,
+} from "../../constants";
 import { getValidationResult, normalizeCategory } from "../../helpers";
 import { Category } from "../../models";
 import CustomError from "../../utils/custom-error";
@@ -42,8 +46,8 @@ export const getAllCategories = async (
   next: NextFunction,
 ) => {
   try {
-    const page = Number(request.query.page) || 1;
-    const limit = Number(request.query.limit) || 10;
+    const page = Number(request.query.page);
+    const limit = Number(request.query.limit);
     const skip = (page - 1) * limit;
 
     const totalCategories = await Category.countDocuments();
@@ -54,6 +58,37 @@ export const getAllCategories = async (
       .transform((document) => document.map((document) => document.toObject()));
 
     response.status(200).json({ categories, total: totalCategories });
+  } catch {
+    const error = new CustomError(ServerErrorMessage);
+    next(error);
+  }
+};
+
+export const getSingleCategory = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  try {
+    const validationErrors = getValidationResult(request);
+
+    if (validationErrors) {
+      const error = new CustomError(
+        ValidationErrorMessage,
+        400,
+        validationErrors,
+      );
+      return next(error);
+    }
+
+    const category = await Category.findOne({ _id: request.params.categoryId });
+
+    if (!category)
+      return response
+        .status(404)
+        .json({ message: getNotFoundMessage("Category") });
+
+    response.status(200).json(category.toObject());
   } catch {
     const error = new CustomError(ServerErrorMessage);
     next(error);
