487a566fd632c741f756ac7640f9b2bbdc2993b1
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-08 09:06:19 +0200

    refactor: make page and limit query properties optional

--- src/validations/get-all.validation.ts
+++ src/validations/get-all.validation.ts
@@ -2,7 +2,11 @@ import { query } from "express-validator";
 
 export default function getAllSanitizer(limit: number = 10) {
   return [
-    query("page").customSanitizer((value) => Number(value) || 1),
-    query("limit").customSanitizer((value) => Number(value) || limit),
+    query("page")
+      .custom((value) => Number(value) || 1)
+      .optional({ values: "undefined" }),
+    query("limit")
+      .custom((value) => Number(value) || limit)
+      .optional({ values: "undefined" }),
   ];
 }
