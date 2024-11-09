35c8ca938556ba83ec2272d4bdcd372fde63fefe
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-09 13:32:06 +0200

    refactor: add aws environment variables

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -9,4 +9,8 @@ export default {
   resendApiKey: process.env.RESEND_API_KEY ?? "",
   callbackToken: process.env.CALLBACK_TOKEN ?? "",
   jwtSecret: process.env.JWT_SECRET_KEY ?? "",
+  awsBucketName: process.env.AWS_BUCKET_NAME ?? "",
+  awsRegion: process.env.AWS_REGION ?? "",
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
 };
