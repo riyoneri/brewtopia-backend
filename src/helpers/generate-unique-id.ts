39bd384ed84f7f074884ecdf081ad7d8fb8d8ea2
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-09 17:25:59 +0200

    feat: create helper function for generating image unique id

--- src/helpers/generate-unique-id.ts
+++ src/helpers/generate-unique-id.ts
@@ -3,3 +3,5 @@ import { nanoid } from "nanoid";
 export const getVerifyEmailUniqueId = () => nanoid(25);
 
 export const getResetPasswordUniqueId = () => nanoid(30);
+
+export const getProductImageUniqueId = () => nanoid(25);
