4653f48a7378f983ce66eca1eb7c6d71abef637d
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-12 10:42:29 +0200

    feat: create update product admin controller

--- src/controllers/admin/products.controller.ts
+++ src/controllers/admin/products.controller.ts
@@ -62,7 +62,7 @@ export const createProduct = async (
       imageUrl: `${environment.awsS3Url}/${imagePath}`,
       price: request.body.price,
       description: request.body.description,
-      category: request.body.category,
+      category: selectedCategory,
     });
 
     const savedProduct = await newProductData.save();
@@ -133,6 +133,99 @@ export const getSingleProduct = async (
 
     response.status(200).json(product.toObject());
   } catch {
+    const error = new CustomError(ServerErrorMessage);
+    next(error);
+  }
+};
+
+export const updateProduct = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  let imagePath = "";
+  const s3 = getS3Client();
+  let isProductUpdated = false;
+
+  try {
+    const validationErrors = getValidationResult(request);
+
+    if (validationErrors) {
+      const error = new CustomError(
+        ValidationErrorMessage,
+        400,
+        validationErrors,
+      );
+      return next(error);
+    }
+
+    const product = await Product.findById(request.params.productId);
+
+    if (!product) {
+      const error = new CustomError(getNotFoundMessage("Product"), 404);
+
+      return next(error);
+    }
+
+    const selectedCategory = await Category.findById(request.body.category);
+
+    if (!selectedCategory) {
+      const error = new CustomError(ValidationErrorMessage, 400, {
+        category: getNotFoundMessage("Category"),
+      });
+
+      return next(error);
+    }
+
+    if (Buffer.isBuffer(request.body.image)) {
+      const resizedImage = await sharp(request.body.image)
+        .resize(1000, undefined, { withoutEnlargement: true })
+        .toBuffer();
+
+      imagePath = `products/${getProductImageUniqueId()}.webp`;
+
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: environment.awsBucketName,
+          Body: resizedImage,
+          Key: imagePath,
+        }),
+      );
+    }
+
+    const previousImageKey = product.imageUrl.split(
+      `${environment.awsS3Url}/`,
+    )[1];
+
+    product.name = request.body.name;
+    product.imageUrl = imagePath
+      ? `${environment.awsS3Url}/${imagePath}`
+      : request.body.image;
+    product.price = request.body.price;
+    product.description = request.body.description;
+    product.category = selectedCategory.id;
+
+    const updatedProduct = await product.save({ validateModifiedOnly: true });
+    isProductUpdated = true;
+
+    await s3.send(
+      new DeleteObjectCommand({
+        Bucket: environment.awsBucketName,
+        Key: previousImageKey,
+      }),
+    );
+
+    response.status(200).json(updatedProduct.toObject());
+  } catch {
+    if (imagePath && !isProductUpdated) {
+      await s3.send(
+        new DeleteObjectCommand({
+          Bucket: environment.awsBucketName,
+          Key: imagePath,
+        }),
+      );
+    }
+
     const error = new CustomError(ServerErrorMessage);
     next(error);
   }
