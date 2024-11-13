9611335022430bcebe3c5a9971594c0fcee1debd
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-13 08:01:21 +0200

    feat: create admin route for deleting a product

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -14,6 +14,7 @@ import {
   createCategoryChain,
   createProductChain,
   deleteCategoryChain,
+  deleteProductChain,
   getAllSanitizer,
   getSingleCategoryChain,
   getSingleProductChain,
@@ -81,6 +82,11 @@ router
     productFileUploadMiddleware,
     updateProductChain(),
     productsController.updateProduct,
+  )
+  .delete(
+    "/products/:productId",
+    deleteProductChain(),
+    productsController.deleteProduct,
   );
 
 export default router;
